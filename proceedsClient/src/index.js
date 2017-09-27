57d276eea42e0c6e698b8758cb7f1b324c9c9342
Brett Newlund <bmnewlund@gmail.com>
2017-09-27 14:54:38 -0400

    Setup Client

---
+++ proceedsClient/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+ReactDOM.render(
+  	<Provider store={createStoreWithMiddleware(reducers)}>
+    	<Router history={browserHistory}>
+        	<Route path="/" component={App}>
+            	<Route path="signin" component={Signin} />
+            	<Route path="signout" component={Signout} />
+            	<Route path="signup" component={Signup} />
+        	</Route>
+      	</Router>
+ 	</Provider>
+  , document.querySelector('.container'));
